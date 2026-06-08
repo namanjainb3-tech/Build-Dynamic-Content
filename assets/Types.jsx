ef9f6fe97bc72dce7b20c7dfda506e0b536f164e
Naman Jain <namanjainb3@gmail.com>
2026-06-08 11:54:00 +0530

    Delete assets directory

--- assets/Types.jsx
+++
@@ -1,8 +0,0 @@
-export const TYPES = {
-    HEADER: 'header',
-    TEXT: 'text',
-    IMAGE: 'image'
-  };
-  
-  export const STORAGE_KEY = 'builder-pro-data';
-  export const THEME_KEY = 'builder-theme';
